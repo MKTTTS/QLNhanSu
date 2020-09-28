b314f2b6710aa33eeab1bdff17756d4b5978365c
TLGKIEN <kienmathip1999@gmail.com>
2020-09-28 15:27:30 +0700

    finish login

---
+++ QLNhanSu/BUS/BUS_Login.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DAO;
+
+namespace BUS
+{
+    public class BUS_Login
+    {
+        private static BUS_Login instance;
+
+        public static BUS_Login Instance
+        {
+            get { if (instance == null) instance = new BUS_Login(); return instance; }
+            private set { instance = value; }
+        }
+
+        private BUS_Login() { }
+
+        public string CheckLogin(object TenDN, object MatKhau)
+        {
+            return DataProvider.ExecuteScalar("SP_Login", CommandType.StoredProcedure, new string[] { "@TenDN", "@MatKhau" }, new object[] { TenDN, MatKhau });
+        }
+
+    }
+}
