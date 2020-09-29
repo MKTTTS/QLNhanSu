60c390513079a56583ec56d154fcc64607cce28c
trivan77 <o868994610@gmail.com>
2020-09-29 15:38:15 +0700

    test

--- QLNhanSu/View/TimKiemNhanVien.Designer.cs
+++ QLNhanSu/View/TimKiemNhanVien.Designer.cs
@@ -28,20 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // TimKiemNhanVien
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1000, 650);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "TimKiemNhanVien";
-            this.Text = "TimKiemNhanVien";
-            this.ResumeLayout(false);
+               this.button1 = new System.Windows.Forms.Button();
+               this.SuspendLayout();
+               // 
+               // button1
+               // 
+               this.button1.Location = new System.Drawing.Point(123, 109);
+               this.button1.Name = "button1";
+               this.button1.Size = new System.Drawing.Size(75, 23);
+               this.button1.TabIndex = 0;
+               this.button1.Text = "button1";
+               this.button1.UseVisualStyleBackColor = true;
+               // 
+               // TimKiemNhanVien
+               // 
+               this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+               this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+               this.ClientSize = new System.Drawing.Size(1000, 650);
+               this.Controls.Add(this.button1);
+               this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+               this.Name = "TimKiemNhanVien";
+               this.Text = "TimKiemNhanVien";
+               this.ResumeLayout(false);
 
         }
 
-        #endregion
-    }
+          #endregion
+
+          private System.Windows.Forms.Button button1;
+     }
 }
