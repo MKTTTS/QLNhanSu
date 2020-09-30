b9c9780ceeb4a9801b47387f22ed5a465088ed82
trivan77 <o868994610@gmail.com>
2020-09-30 14:40:57 +0700

    update timkiem 30/9

--- QLNhanSu/View/TimKiemNhanVien.cs
+++ QLNhanSu/View/TimKiemNhanVien.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace View
 {
@@ -16,5 +18,33 @@ namespace View
         {
             InitializeComponent();
         }
-    }
+
+          private void cbxTieuChi_TextChanged(object sender, EventArgs e)
+          {
+               string conString = ConfigurationManager.ConnectionStrings["myconnection"].ConnectionString;
+               SqlConnection sqlCon = new SqlConnection(conString);
+               sqlCon.Open();
+
+
+               if(cbxTieuChi.Text=="Chức Vụ")
+               {
+                    cbxDeMuc.Text = "1";
+               }
+
+               if (cbxTieuChi.Text == "Bằng Cấp")
+               {
+                    cbxDeMuc.Text = "2";
+               }
+
+               if (cbxTieuChi.Text == "Phòng Ban")
+               {
+                    cbxDeMuc.Text = "3";
+               }
+
+               if (cbxTieuChi.Text == "Địa Chỉ")
+               {
+                    cbxDeMuc.Text = "4";
+               }
+          }
+     }
 }
