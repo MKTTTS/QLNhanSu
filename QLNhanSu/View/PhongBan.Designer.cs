de02818204295cf77534cb74b3ed583944ae295b
DangT13n <dangtien26021999@gmail.com>
2020-10-02 20:40:16 +0700

    tienupdate0210

--- QLNhanSu/View/PhongBan.Designer.cs
+++ QLNhanSu/View/PhongBan.Designer.cs
@@ -28,20 +28,291 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.TenTP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tenPB = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btXoa = new System.Windows.Forms.Button();
+            this.btSua = new System.Windows.Forms.Button();
+            this.btThem = new System.Windows.Forms.Button();
+            this.maPB = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.maTP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btSearch = new System.Windows.Forms.Button();
+            this.lbTenTp = new System.Windows.Forms.Label();
+            this.lbTenPB = new System.Windows.Forms.Label();
+            this.lbMPB = new System.Windows.Forms.Label();
+            this.lbQLPB = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbMaTP = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // TenTP
+            // 
+            this.TenTP.HeaderText = "Tên Trưởng phòng";
+            this.TenTP.MinimumWidth = 6;
+            this.TenTP.Name = "TenTP";
+            this.TenTP.Width = 200;
+            // 
+            // tenPB
+            // 
+            this.tenPB.HeaderText = "Tên Phòng ban";
+            this.tenPB.MinimumWidth = 6;
+            this.tenPB.Name = "tenPB";
+            this.tenPB.Width = 200;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(214, 253);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(210, 27);
+            this.textBox5.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(638, 170);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(219, 27);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(165, 170);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(210, 27);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(638, 104);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(219, 27);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(165, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(210, 27);
+            this.textBox1.TabIndex = 9;
+            // 
+            // btXoa
+            // 
+            this.btXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btXoa.Location = new System.Drawing.Point(782, 253);
+            this.btXoa.Name = "btXoa";
+            this.btXoa.Size = new System.Drawing.Size(75, 27);
+            this.btXoa.TabIndex = 8;
+            this.btXoa.Text = "Xóa";
+            this.btXoa.UseVisualStyleBackColor = true;
+            // 
+            // btSua
+            // 
+            this.btSua.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSua.Location = new System.Drawing.Point(656, 253);
+            this.btSua.Name = "btSua";
+            this.btSua.Size = new System.Drawing.Size(75, 27);
+            this.btSua.TabIndex = 7;
+            this.btSua.Text = "Sửa";
+            this.btSua.UseVisualStyleBackColor = true;
+            // 
+            // btThem
+            // 
+            this.btThem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btThem.Location = new System.Drawing.Point(538, 253);
+            this.btThem.Name = "btThem";
+            this.btThem.Size = new System.Drawing.Size(75, 27);
+            this.btThem.TabIndex = 6;
+            this.btThem.Text = "Thêm";
+            this.btThem.UseVisualStyleBackColor = true;
+            // 
+            // maPB
+            // 
+            this.maPB.HeaderText = "Mã Phòng ban";
+            this.maPB.MinimumWidth = 6;
+            this.maPB.Name = "maPB";
+            this.maPB.Width = 200;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.maPB,
+            this.tenPB,
+            this.maTP,
+            this.TenTP});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(950, 357);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // maTP
+            // 
+            this.maTP.HeaderText = "Mã Trưởng phòng";
+            this.maTP.MinimumWidth = 6;
+            this.maTP.Name = "maTP";
+            this.maTP.Width = 200;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Location = new System.Drawing.Point(106, 345);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(953, 357);
+            this.panel2.TabIndex = 1;
+            // 
+            // btSearch
+            // 
+            this.btSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSearch.Location = new System.Drawing.Point(77, 253);
+            this.btSearch.Name = "btSearch";
+            this.btSearch.Size = new System.Drawing.Size(100, 27);
+            this.btSearch.TabIndex = 5;
+            this.btSearch.Text = "Tìm kiếm";
+            this.btSearch.UseVisualStyleBackColor = true;
+            // 
+            // lbTenTp
+            // 
+            this.lbTenTp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTenTp.Location = new System.Drawing.Point(526, 170);
+            this.lbTenTp.Name = "lbTenTp";
+            this.lbTenTp.Size = new System.Drawing.Size(73, 17);
+            this.lbTenTp.TabIndex = 4;
+            this.lbTenTp.Text = "Tên TP";
+            // 
+            // lbTenPB
+            // 
+            this.lbTenPB.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTenPB.Location = new System.Drawing.Point(73, 170);
+            this.lbTenPB.Name = "lbTenPB";
+            this.lbTenPB.Size = new System.Drawing.Size(75, 17);
+            this.lbTenPB.TabIndex = 3;
+            this.lbTenPB.Text = "Tên PB";
+            // 
+            // lbMPB
+            // 
+            this.lbMPB.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMPB.Location = new System.Drawing.Point(77, 104);
+            this.lbMPB.Name = "lbMPB";
+            this.lbMPB.Size = new System.Drawing.Size(71, 17);
+            this.lbMPB.TabIndex = 1;
+            this.lbMPB.Text = "Mã PB";
+            // 
+            // lbQLPB
+            // 
+            this.lbQLPB.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbQLPB.Location = new System.Drawing.Point(333, 19);
+            this.lbQLPB.Name = "lbQLPB";
+            this.lbQLPB.Size = new System.Drawing.Size(246, 34);
+            this.lbQLPB.TabIndex = 0;
+            this.lbQLPB.Text = "Quản ý Phòng Ban";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.btXoa);
+            this.panel1.Controls.Add(this.btSua);
+            this.panel1.Controls.Add(this.btThem);
+            this.panel1.Controls.Add(this.btSearch);
+            this.panel1.Controls.Add(this.lbTenTp);
+            this.panel1.Controls.Add(this.lbTenPB);
+            this.panel1.Controls.Add(this.lbMaTP);
+            this.panel1.Controls.Add(this.lbMPB);
+            this.panel1.Controls.Add(this.lbQLPB);
+            this.panel1.Location = new System.Drawing.Point(106, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(953, 336);
+            this.panel1.TabIndex = 0;
+            // 
+            // lbMaTP
+            // 
+            this.lbMaTP.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMaTP.Location = new System.Drawing.Point(534, 104);
+            this.lbMaTP.Name = "lbMaTP";
+            this.lbMaTP.Size = new System.Drawing.Size(65, 17);
+            this.lbMaTP.TabIndex = 2;
+            this.lbMaTP.Text = "Mã TP";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.756098F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 90.2439F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 63F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 2);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48.65248F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 51.34752F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1126, 705);
+            this.tableLayoutPanel1.TabIndex = 1;
             // 
             // PhongBan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1000, 650);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.ClientSize = new System.Drawing.Size(1126, 708);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "PhongBan";
             this.Text = "PhongBan";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenTP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tenPB;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btXoa;
+        private System.Windows.Forms.Button btSua;
+        private System.Windows.Forms.Button btThem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn maPB;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn maTP;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btSearch;
+        private System.Windows.Forms.Label lbTenTp;
+        private System.Windows.Forms.Label lbTenPB;
+        private System.Windows.Forms.Label lbMPB;
+        private System.Windows.Forms.Label lbQLPB;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbMaTP;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
