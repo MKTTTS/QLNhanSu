4b7bb96a0aaa8b6e3e8a16b2a92ea35e7e7ca648
Nguyễn Mạnh Sơn <56079016+sonvbhp199@users.noreply.github.com>
2020-10-19 22:35:04 +0700

    vitri cong viec

---
+++ Vitri.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace View
+{
+    partial class Vitri
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Vitri));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgVT = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtMaVT = new System.Windows.Forms.TextBox();
+            this.txtTenVT = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_EditVT = new System.Windows.Forms.Button();
+            this.btn_AddVT = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgVT)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgVT);
+            this.groupBox2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(367, 98);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(415, 423);
+            this.groupBox2.TabIndex = 20;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Danh sách";
+            // 
+            // dgVT
+            // 
+            this.dgVT.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgVT.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgVT.Location = new System.Drawing.Point(11, 18);
+            this.dgVT.Name = "dgVT";
+            this.dgVT.RowHeadersWidth = 51;
+            this.dgVT.Size = new System.Drawing.Size(398, 399);
+            this.dgVT.TabIndex = 0;
+            this.dgVT.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgVT_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(294, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(302, 35);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "QUẢN LÝ CHỨC VỤ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtMaVT);
+            this.groupBox1.Controls.Add(this.txtTenVT);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(21, 98);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(321, 423);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Lựa chọn";
+            // 
+            // txtMaVT
+            // 
+            this.txtMaVT.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaVT.Location = new System.Drawing.Point(115, 46);
+            this.txtMaVT.Name = "txtMaVT";
+            this.txtMaVT.Size = new System.Drawing.Size(179, 26);
+            this.txtMaVT.TabIndex = 1;
+            // 
+            // txtTenVT
+            // 
+            this.txtTenVT.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTenVT.Location = new System.Drawing.Point(115, 96);
+            this.txtTenVT.Name = "txtTenVT";
+            this.txtTenVT.Size = new System.Drawing.Size(179, 26);
+            this.txtTenVT.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(19, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 19);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Tên vị trí";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(19, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 19);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mã vị trí";
+            // 
+            // btn_save
+            // 
+            this.btn_save.Image = global::View.Properties.Resources.Save_24x24;
+            this.btn_save.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_save.Location = new System.Drawing.Point(831, 370);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(92, 39);
+            this.btn_save.TabIndex = 23;
+            this.btn_save.Text = "Lưu";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_EditVT
+            // 
+            this.btn_EditVT.Image = ((System.Drawing.Image)(resources.GetObject("btn_EditVT.Image")));
+            this.btn_EditVT.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_EditVT.Location = new System.Drawing.Point(831, 274);
+            this.btn_EditVT.Name = "btn_EditVT";
+            this.btn_EditVT.Size = new System.Drawing.Size(92, 39);
+            this.btn_EditVT.TabIndex = 22;
+            this.btn_EditVT.Text = "Sửa";
+            this.btn_EditVT.UseVisualStyleBackColor = true;
+            this.btn_EditVT.Click += new System.EventHandler(this.btn_EditVT_Click);
+            // 
+            // btn_AddVT
+            // 
+            this.btn_AddVT.Image = ((System.Drawing.Image)(resources.GetObject("btn_AddVT.Image")));
+            this.btn_AddVT.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_AddVT.Location = new System.Drawing.Point(831, 188);
+            this.btn_AddVT.Name = "btn_AddVT";
+            this.btn_AddVT.Size = new System.Drawing.Size(92, 39);
+            this.btn_AddVT.TabIndex = 21;
+            this.btn_AddVT.Text = "Thêm";
+            this.btn_AddVT.UseVisualStyleBackColor = true;
+            this.btn_AddVT.Click += new System.EventHandler(this.btn_AddVT_Click);
+            // 
+            // Vitri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(966, 559);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_EditVT);
+            this.Controls.Add(this.btn_AddVT);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Vitri";
+            this.Text = "Vitri";
+            this.Load += new System.EventHandler(this.Vitri_Load);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgVT)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_EditVT;
+        private System.Windows.Forms.Button btn_AddVT;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgVT;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtMaVT;
+        private System.Windows.Forms.TextBox txtTenVT;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_save;
+    }
+}
---
+++ Vitri.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace View
+{
+    public partial class Vitri : Form
+    {
+        string flag;
+        SqlConnection connection;
+        SqlCommand command;
+        string str = @"Data Source=DESKTOP-85J13SR;Initial Catalog=QuanLyNhanSu;Integrated Security=True";
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        System.Data.DataTable table = new System.Data.DataTable();
+        void hienthi()
+        {
+            connection.Open();
+            command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM dbo.VITRICONGVIEC";
+            adapter.SelectCommand = command;
+            table.Clear();
+            adapter.Fill(table);
+            dgVT.DataSource = table;
+            dgVT.ReadOnly = true;
+            connection.Close();
+        }
+        public Vitri()
+        {
+            InitializeComponent();
+        }
+
+        private void Vitri_Load(object sender, EventArgs e)
+        {
+            lockcontrol();
+            connection = new SqlConnection(str);
+            hienthi();
+        }
+        public void lockcontrol()
+        {
+            btn_save.Enabled = false;
+            txtMaVT.ReadOnly = true;
+            txtTenVT.ReadOnly = true;
+        }
+        public void unlock()
+        {
+            txtMaVT.ReadOnly = true;
+            txtTenVT.ReadOnly = false;
+            btn_save.Enabled = true;
+        }
+
+        private void dgVT_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            lockcontrol();
+            int i;
+            i = dgVT.CurrentRow.Index;
+            txtMaVT.Text = dgVT.Rows[i].Cells[0].Value.ToString();
+            txtTenVT.Text = dgVT.Rows[i].Cells[1].Value.ToString();
+        }
+
+        private void btn_AddVT_Click(object sender, EventArgs e)
+        {
+            unlock();
+            txtMaVT.ReadOnly = false;
+            flag = "add";
+            txtMaVT.Text = "";
+            txtTenVT.Text = "";
+        }
+
+        private void btn_EditVT_Click(object sender, EventArgs e)
+        {
+            unlock();
+            flag = "edit";
+        }
+        public bool check()
+        {
+            if (string.IsNullOrEmpty(txtMaVT.Text))
+            {
+                MessageBox.Show("Chưa nhập mã vị trí", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtMaVT.Focus();
+                return false;
+            }
+            if (string.IsNullOrEmpty(txtTenVT.Text))
+            {
+                MessageBox.Show("Chưa nhập tên vị trí", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtTenVT.Focus();
+                return false;
+            }
+            return true;
+        }
+        public bool check1(string mavt)
+        {
+            connection.Open();
+            command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM dbo.VITRICONGVIEC";
+            adapter.SelectCommand = command;
+            int k = 0;
+            DataTable tb = new DataTable();
+            adapter.Fill(tb);
+            for (int i = 0; i < tb.Rows.Count; i++)
+            {
+                if (mavt == tb.Rows[i].ItemArray[0].ToString().Trim())
+                {
+                    k++;
+                    break;
+                }
+            }
+            connection.Close();
+            if (k > 0)
+            {
+                MessageBox.Show("Mã vị trí đã tồn tại", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtMaVT.Focus();
+                return false;
+            }
+            else return true;
+        }
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            if (flag == "add")
+            {
+                if (check() && check1(txtMaVT.Text))
+                {
+                    connection.Open();
+                    command = connection.CreateCommand();
+                    command.CommandText = "INSERT INTO dbo.VITRICONGVIEC (MaVT,TenVT) VALUES ('"+txtMaVT.Text+"','"+txtTenVT.Text+"')";
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                    hienthi();
+                }
+            }
+            else if(flag=="edit")
+            {
+                connection.Open();
+                command = connection.CreateCommand();
+                command.CommandText = "UPDATE dbo.VITRICONGVIEC SET TenVT = '"+txtTenVT.Text+"' WHERE MaVT = '"+txtMaVT.Text+"'";
+                command.ExecuteNonQuery();
+                connection.Close();
+                hienthi();
+            }
+        }
+    }
+}
